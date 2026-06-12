163e9120de98682fb4cb9dfd0a1c32ecdd13abfa
Riddler <winhtutooheart2@gmail.com>
2026-06-12 15:26:33 +0900

    maybe

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../public',
+    emptyOutDir: true, // clears old files automatically before each build
+  },
   server: {
     port: 5173,
     proxy: {
